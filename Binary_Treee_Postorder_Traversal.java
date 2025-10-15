ca894f54dd65a351b477b0ef4110c46c092583e9
sarkaul2 <lukas.sarkauskas2@mail.dcu.ie>
2025-10-15 05:05:55 +0100

    Solved java Problems

---
+++ Binary_Treee_Postorder_Traversal.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(root);
+
+        while(!stack1.isEmpty()) {
+            TreeNode curr = stack1.pop();
+            stack2.push(curr);
+
+            if(curr.left != null) stack1.push(curr.left);
+            if(curr.right != null) stack1.push(curr.right);
+        }
+
+        while(!stack2.isEmpty()) {
+            result.add(stack2.pop().val); // now nodes are in postorder
+        }
+        return result;
+    }
+}
